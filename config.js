8880f75167a0301446c5c3c5e128227227f4223a
Andrew Balmos <andrew@balmos.org>
2017-05-06 10:17:57 -0400

    Make nconf work when not nested in node_modules

--- config.js
+++ config.js
@@ -14,6 +14,13 @@
  */
 'use strict';
 
+// We use nconf in dependencies, so we have to play games here. If npm/yarn
+// factors nconf up node_modules then our dependencies get the same nconf
+// instance as we do, allowing them to mutate each others keys. This ensures we
+// get our own nconf --- yes it is ugly. Thank you node.js for assuming we
+// always want your hidden cache.
+delete require.cache[require.resolve('nconf')];
+
 var nconf = require('nconf');
 var fs = require('fs');
 var _ = require('lodash');
@@ -21,53 +28,19 @@ var debug = require('debug')('info');
 
 nconf.use('memory');
 
-/*
 // Order of precedence: argv, env, config file, defaults
 nconf.argv();
 nconf.env('_');
 
-// Load an external (optional) config file from command-line args, but
-// only if we're running standalone and not included as a library.
-// index.js sets this global variable since it's the only one that knows
-// if it's a library or not.
+// Load an external (optional) config file from command-line args
 var config = nconf.get('config-arango');
 if (config) {
   if (!fs.existsSync(config)) {
     throw new Error('Could not find config file: ' + config);
   }
-  if (config.match(/\.json$/)) {
-    nconf.file(config);
-  } else {
-    // .JS file instead, so require it rather than nconf.file it:
-    nconf.use('literal', require(config));
-  }
+  nconf.use('literal', require(config));
 }
-*/
 
 nconf.defaults(require('./config.defaults.js'));
-
-nconf.setObject = function(object, path) {
-  path = path || '';
-
-  Object.keys(object).forEach(function(key) {
-    if(typeof object[key] === '[object]') {
-      nconf.setObject(object[key], path + key + ':');
-    } else {
-      nconf.set(path + key, object[key]);
-    }
-  });
-}
-
-// If there is an endpointsPrefix, update all the endpoints to include the
-// prefix before returning:
-const pfx = nconf.get('endpointsPrefix');
-if (typeof pfx === 'string') {
-  debug('config: Adding supplied prefix '+pfx+' to all endpoints');
-  const endpoints = nconf.get('endpoints');
-  _.mapValues(endpoints, (v,k) => {
-    nconf.set('endpoints:'+k, pfx+v);
-  });
-}
-
 
 module.exports = nconf;
